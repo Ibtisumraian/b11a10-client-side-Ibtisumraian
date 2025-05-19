eb5015676354f43911162d009e085657ea1eb2ae
Ibtisumraian <ibtisumraian@gmail.com>
2025-05-20 01:16:26 +0600

    added static top recipes sectin, yet to update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
+import TopRecipes from '../../components/TopRecipes/TopRecipes';
 
 const Home = () => {
     return (
         <div>
-            <Banner></Banner>
+            <div>
+                <Banner></Banner>
+            </div>
+            <div>
+                <TopRecipes></TopRecipes>
+            </div>
         </div>
     );
 };
---
+++ src/components/TopRecipes/TopRecipes.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import { FaHeart } from "react-icons/fa6";
+
+const TopRecipes = () => {
+    return (
+        <div className='w-9/12 mx-auto '>
+            <div className='text-center my-10'>
+                <h1 className='text-5xl font-bold fontRokkitt'>Top Recipes</h1>
+            </div>
+            <div className='grid grid-cols-3 gap-8'>
+                <div className="card bg-base-100 w-full shadow-sm">
+            <figure className="px-10 pt-10">
+                <img
+                src="https://res.cloudinary.com/dd4np04jl/image/upload/v1747409610/Rectangle_16_pnutvj.png"
+                alt="Shoes"
+                className="rounded-xl" />
+            </figure>
+            <div className="card-body text-center">
+                <h2 className="">Chicken Tikka Masala</h2>
+                <p>Mexican</p>
+                <p className='text-[#005A52]'><FaHeart /></p>
+                <div className="card-actions">
+                <button className="btn btn-wide bg-[#005A52] text-white mx-auto">View Details</button>
+                </div>
+            </div>
+            </div>
+            {/* 2 */}
+             <div className="card bg-base-100 w-full shadow-sm">
+            <figure className="px-10 pt-10">
+                <img
+                src="https://res.cloudinary.com/dd4np04jl/image/upload/v1747409610/Rectangle_16_pnutvj.png"
+                alt="Shoes"
+                className="rounded-xl" />
+            </figure>
+            <div className="card-body text-center">
+                <h2 className="">Chicken Tikka Masala</h2>
+                <p>Mexican</p>
+                <p className='text-[#005A52]'><FaHeart /></p>
+                <div className="card-actions">
+                <button className="btn btn-wide bg-[#005A52] text-white mx-auto">View Details</button>
+                </div>
+            </div>
+            </div>
+            {/*  */}
+             <div className="card bg-base-100 w-full shadow-sm">
+            <figure className="px-10 pt-10">
+                <img
+                src="https://res.cloudinary.com/dd4np04jl/image/upload/v1747409610/Rectangle_16_pnutvj.png"
+                alt="Shoes"
+                className="rounded-xl" />
+            </figure>
+            <div className="card-body text-center">
+                <h2 className="">Chicken Tikka Masala</h2>
+                <p>Mexican</p>
+                <p className='text-[#005A52]'><FaHeart /></p>
+                <div className="card-actions">
+                <button className="btn btn-wide bg-[#005A52] text-white mx-auto">View Details</button>
+                </div>
+            </div>
+            </div>
+
+             <div className="card bg-base-100 w-full shadow-sm">
+            <figure className="px-10 pt-10">
+                <img
+                src="https://res.cloudinary.com/dd4np04jl/image/upload/v1747409610/Rectangle_16_pnutvj.png"
+                alt="Shoes"
+                className="rounded-xl" />
+            </figure>
+            <div className="card-body text-center">
+                <h2 className="">Chicken Tikka Masala</h2>
+                <p>Mexican</p>
+                <p className='text-[#005A52]'><FaHeart /></p>
+                <div className="card-actions">
+                <button className="btn btn-wide bg-[#005A52] text-white mx-auto">View Details</button>
+                </div>
+            </div>
+            </div>
+
+             <div className="card bg-base-100 w-full shadow-sm">
+            <figure className="px-10 pt-10">
+                <img
+                src="https://res.cloudinary.com/dd4np04jl/image/upload/v1747409610/Rectangle_16_pnutvj.png"
+                alt="Shoes"
+                className="rounded-xl" />
+            </figure>
+            <div className="card-body text-center">
+                <h2 className="">Chicken Tikka Masala</h2>
+                <p>Mexican</p>
+                <p className='text-[#005A52]'><FaHeart /></p>
+                <div className="card-actions">
+                <button className="btn btn-wide bg-[#005A52] text-white mx-auto">View Details</button>
+                </div>
+            </div>
+            </div>
+
+             <div className="card bg-base-100 w-full shadow-sm">
+            <figure className="px-10 pt-10">
+                <img
+                src="https://res.cloudinary.com/dd4np04jl/image/upload/v1747409610/Rectangle_16_pnutvj.png"
+                alt="Shoes"
+                className="rounded-xl" />
+            </figure>
+            <div className="card-body text-center ">
+                <h2 className="">Chicken Tikka Masala</h2>
+                <p>Mexican</p>
+                <p className='text-[#005A52]'><FaHeart /></p>
+                <div className="card-actions">
+                <button className="btn btn-wide bg-[#005A52] text-white mx-auto">View Details</button>
+                </div>
+            </div>
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default TopRecipes;
